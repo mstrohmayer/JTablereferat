5808fca3225d9aa8e9dec459fb00715329e1b373
JUPITER\Matthias <matthias.strohmayer@web.de>
2017-03-19 23:13:30 +0100

    Project für Übung vorbereitet

--- src/com/sabel/jtablereferat/gui/ContactGui.java
+++ src/com/sabel/jtablereferat/gui/ContactGui.java
@@ -18,57 +18,5 @@ import java.awt.event.ActionListener;
  * Created by Matthias on 19.03.2017.
  */
 public class ContactGui extends JFrame {
-    private Container c;
-    private JPanel jpNorth, jpSouth;
-    private ContactTableModel contactTableModel;
-    private JTable jTable;
-    private JScrollPane jScrollPane;
-    private JButton jbNew, jbRemove;
 
-    public ContactGui() throws HeadlessException {
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(500, 500);
-        this.initComponents();
-        this.initEvents();
-
-        this.setVisible(true);
-    }
-
-    private void initComponents() {
-        c = getContentPane();
-
-        jpNorth = new JPanel();
-        contactTableModel = new ContactTableModel();
-        jTable = new JTable(contactTableModel);
-        jTable.setFillsViewportHeight(true);
-        jTable.setDefaultRenderer(String.class,new DefaultTableCellRenderer());
-        jScrollPane = new JScrollPane(jTable);
-        jpNorth.add(jScrollPane);
-
-        jpSouth = new JPanel();
-        jbNew = new JButton("Neuer Kontakt");
-        jbRemove = new JButton("Kontakt löschen");
-        jpSouth.add(jbNew);
-        jpSouth.add(jbRemove);
-
-        c.add(jpNorth, BorderLayout.NORTH);
-        c.add(jpSouth, BorderLayout.SOUTH);
-    }
-
-    private void initEvents() {
-        contactTableModel.addTableModelListener(this.jTable);
-        jbNew.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                contactTableModel.addContact(new Contact());
-            }
-        });
-        jbRemove.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                contactTableModel.removeContact(jTable.getSelectedRow());
-            }
-        });
-
-    }
 }
